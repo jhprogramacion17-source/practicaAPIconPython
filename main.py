c25bb5b542c0ef0702c9d5667f858ec4b42d85db
jhoimer.paladines@unisimon.edu.co <jhprogramacion17@gmail.com>
2026-09-18 19:03:37 -0500

    agregacion de /students

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI(title="students-api", version=APP_VERSION)
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/students")
+def list_students():
+    return [{"id": 1, "name": "Jhon"},{"id": 2, "name": "Doe"}]
+
